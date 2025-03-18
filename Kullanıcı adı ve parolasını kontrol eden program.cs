58eea2be1d182e97265dd4e12f19085cd807c251
iremdegirmencioglu <iremdegirmencioglu403@gmail.com>
2025-03-18 15:36:00 +0300

    Add files via upload

---
+++ Kullanıcı adı ve parolasını kontrol eden program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            string kullanıcı_adı, parola;
+            Console.Write("Kullanıcı Adı:");
+            kullanıcı_adı=Console.ReadLine();
+
+            Console.Write("Şifre:");
+            parola=Console.ReadLine();
+
+            if (kullanıcı_adı == "Ayşe" && parola !=" abcd")
+            {
+                Console.Write("Sisteme başarıyla erişim sağlandı.");
+            }
+
+            else
+            {
+                if(kullanıcı_adı=="Ayşe" && parola !="abcd")
+                  Console.Write("Parola hatalı.");
+
+                if(kullanıcı_adı != "Ayşe" && parola=="abcd")
+                    Console.Write("Kullanıcı Adı hatalı.");
+
+                if (kullanıcı_adı != "Ayşe" && parola != "abcd")
+                    Console.Write("Kullanıcı Adı ve parola hatalı.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
